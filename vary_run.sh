2d6af23c731891a3a193ea0029a743669d457e59
Anup Agarwal <anupa@andrew.cmu.edu>
2020-04-22 19:48:36 -0600

    Add fixed mem tests

--- vary_run.sh
+++ vary_run.sh
@@ -13,9 +13,6 @@ rows=(
     30
     60
     80
-    90
-    200
-    300
 )
 
 # rows=(
@@ -57,32 +54,34 @@ rows=(
 #     15000
 # )
 
-cols=(
-    2048
-)
+# cols=(
+#     2048
+# )
+
 for r in ${rows[@]}; do
 
     sed -i "s/#define NUM_ROWS .*/#define NUM_ROWS $r/g" cm-sketch/sandbox.c
     python update_rows.py $r
 
-    for c in ${cols[@]}; do
+    c=$((4000/$r))
+#   for c in ${cols[@]}; do
 
-        echo "Running for rows, cols: $rows, $c"
-        sed -i "s/#define NUM_COLS .*/#define NUM_COLS $c/g" cm-sketch/sandbox.c
+    echo "Running for rows, cols: $r, $c"
+    sed -i "s/#define NUM_COLS .*/#define NUM_COLS $c/g" cm-sketch/sandbox.c
 
-        ./build.sh cm-sketch
-        ./load.sh cm-sketch
+    ./build.sh cm-sketch
+    ./load.sh cm-sketch
 
-        sudo /users/anup/Netmon/netronome-agilio/dpdk-receiver/build/app/l2fwd \
-             -c 0x5555 \
-             -w 0000:05:08.0 -w 0000:05:08.1 -w 0000:05:08.2 -w 0000:05:08.3 \
-             --socket-mem "256,256" --log-level=8 -- -p 0xF \
-             > "runs/${r}_${c}.log" 2>&1 &
-        pid=$!
+    sudo /users/anup/Netmon/netronome-agilio/dpdk-receiver/build/app/l2fwd \
+         -c 0x5555 \
+         -w 0000:05:08.0 -w 0000:05:08.1 -w 0000:05:08.2 -w 0000:05:08.3 \
+         --socket-mem "256,256" --log-level=8 -- -p 0xF \
+         > "runs/${r}_${c}.log" 2>&1 &
+    pid=$!
 
-        echo "Running dpdk-receiver with pid: $pid"
-        sleep 40
-        sudo kill $pid
+    echo "Running dpdk-receiver with pid: $pid"
+    sleep 40
+    sudo kill $pid
 
-    done
+#   done
 done
