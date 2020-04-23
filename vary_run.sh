3999462dae526e83d750754dc236a8532c904eb5
Anup Agarwal <anupa@andrew.cmu.edu>
2020-04-23 17:13:49 -0600

    Add emem tests, need to re-run imem

--- vary_run.sh
+++ vary_run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-trap "echo Exited!; exit;" SIGINT SIGTERM
-
 set -m
+set -e
+# trap printout SIGINT
+# printout() {
+#     echo "Exiting"
+#     set +m
+#     set +e
+#     exit 1
+# }
 
 rows=(
     1
-    4
-    5
-    6
-    7
-    30
-    60
-    80
 )
 
 # rows=(
@@ -34,15 +33,25 @@ rows=(
 #     300
 # )
 
-# cols=(
-#     1024
-#     3072
-#     8192
-#     10240
-#     20480
-#     51200
-#     245760
-# )
+cols=(
+    1024
+    2048
+    3072
+    8192
+    10240
+    16384
+    20480
+    32768
+    51200
+    65536
+    131072
+    245760
+    262144
+    524288
+    1048576 # 4MB
+    2097152
+    4194304  # 16MB
+)
 
 # cols=(
 #     2048
@@ -59,29 +68,30 @@ rows=(
 # )
 
 for r in ${rows[@]}; do
-
     sed -i "s/#define NUM_ROWS .*/#define NUM_ROWS $r/g" cm-sketch/sandbox.c
     python update_rows.py $r
 
-    c=$((4000/$r))
-#   for c in ${cols[@]}; do
+    for c in ${cols[@]}; do
 
-    echo "Running for rows, cols: $r, $c"
-    sed -i "s/#define NUM_COLS .*/#define NUM_COLS $c/g" cm-sketch/sandbox.c
+        echo "Running for rows, cols: $r, $c"
+        sed -i "s/#define NUM_COLS .*/#define NUM_COLS $c/g" cm-sketch/sandbox.c
 
-    ./build.sh cm-sketch
-    ./load.sh cm-sketch
+        ./build.sh cm-sketch
+        ./load.sh cm-sketch
 
-    sudo /users/anup/Netmon/netronome-agilio/dpdk-receiver/build/app/l2fwd \
-         -c 0x5555 \
-         -w 0000:05:08.0 -w 0000:05:08.1 -w 0000:05:08.2 -w 0000:05:08.3 \
-         --socket-mem "256,256" --log-level=8 -- -p 0xF \
-         > "runs/${r}_${c}.log" 2>&1 &
-    pid=$!
+        sudo /users/anup/Netmon/netronome-agilio/dpdk-receiver/build/app/l2fwd \
+             -c 0x5555 \
+             -w 0000:05:08.0 -w 0000:05:08.1 -w 0000:05:08.2 -w 0000:05:08.3 \
+             --socket-mem "256,256" --log-level=8 -- -p 0xF \
+             > "runs/${r}_${c}.log" 2>&1 &
+        pid=$!
 
-    echo "Running dpdk-receiver with pid: $pid"
-    sleep 40
-    sudo kill $pid
+        echo "Running dpdk-receiver with pid: $pid"
+        sleep 40
+        sudo kill $pid
 
-#   done
+    done
 done
+
+set +m
+set +e
